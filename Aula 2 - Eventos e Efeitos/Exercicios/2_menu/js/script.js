9cafe0c7062be3ab1af7937ec1ec6c7be4ec3258
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-05-12 13:46:27 -0300

    Atualização do repositório e resolução de exercício 01 - aula 02

---
+++ Aula 2 - Eventos e Efeitos/Exercicios/2_menu/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  $("#botaoAbrir").click(function () {
+    $("#menulateral").fadeToggle('fast');
+  })
+
+  $("#botaoFechar").click(function () {
+    $("#menulateral").fadeToggle('fast');
+  })
+
+})
